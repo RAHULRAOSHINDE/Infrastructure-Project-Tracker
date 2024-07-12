55e917af8bd9451b3e301cd823928d2caab8556f
root <root@ip-10-0-35-218.ap-southeast-1.compute.internal>
2024-07-12 07:27:42 +0000

    Added fairfield database trigger

---
+++ trigger/fairfield_trigger.sql
@@ -0,0 +1,60 @@
+
+USE infraDB;
+
+DELIMITER //
+
+CREATE TRIGGER `fairfield_insert`
+AFTER INSERT ON `fairfield_source`
+FOR EACH ROW 
+BEGIN 
+    INSERT INTO `fairfield_cdc` (
+        `action`,
+        original_id,
+        aug_id,
+        country_name,
+        country_code,
+        map_coordinates,
+        url,
+        region_name,
+        region_code,
+        title,
+        description,
+        status,
+        stages,
+        published_date,
+        procurement_method,
+        budget,
+        currency,
+        buyer,
+        sector,
+        subsector,
+        location,
+        last_modified_dt
+    )
+    VALUES (
+        'insert',
+        NEW.original_id,
+        NEW.aug_id,
+        NEW.country_name,
+        NEW.country_code,
+        NEW.map_coordinates,
+        NEW.url,
+        NEW.region_name,
+        NEW.region_code,
+        NEW.title,
+        NEW.description,
+        NEW.status,
+        NEW.stages,
+        NEW.published_date,
+        NEW.procurement_method,
+        NEW.budget,
+        NEW.currency,
+        NEW.buyer,
+        NEW.sector,
+        NEW.subsector,
+        NEW.location,
+        NOW()
+    );
+END;
+//
+DELIMITER ;
